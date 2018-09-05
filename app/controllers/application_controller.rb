078f624d46286447db81a1ffdfc3a3cd392507bf
Kayla Cross <kac288@georgetown.edu>
2018-09-05 00:39:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
+    @post.save
     redirect '/posts/:id'
   end
 
